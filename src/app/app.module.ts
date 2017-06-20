4e0d967e4436bbb0529a1ee5913738fb53862389
Ryan McCormick <ryan@itjobsguy.com>
2017-06-19 21:46:04 -0500

    add core module and rxjs imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import './rxjs-operators';
+
+import { CoreModule } from './core/core.module';
 import { LayoutModule } from './layout/layout.module';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeModule } from './home/home.module';
 import { JobsModule } from './jobs/jobs.module';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +18,8 @@ import { JobsModule } from './jobs/jobs.module';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    CoreModule,
     LayoutModule,
     HomeModule,
     JobsModule,
