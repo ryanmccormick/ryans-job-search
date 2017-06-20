c5c4b7625c2caaa4fa79ca5eb19bcc93e1c26e5f
Ryan McCormick <ryan@itjobsguy.com>
2017-06-19 19:33:15 -0500

    wire up routed modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeModule } from './home/home.module';
+import { JobsModule } from './jobs/jobs.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +14,8 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     LayoutModule,
+    HomeModule,
+    JobsModule,
     AppRoutingModule
   ],
   providers: [],
