e36d692acdfb5c46bfce2f9a63cce664024fef35
Ryan McCormick <ryan@itjobsguy.com>
2017-06-19 21:48:07 -0500

    other updates

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 
-const layoutComponents = [ NavbarComponent ];
+export const layoutComponents = [ NavbarComponent ];
 
 @NgModule({
   imports: [
